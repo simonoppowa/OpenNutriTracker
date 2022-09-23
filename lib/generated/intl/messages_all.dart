04454d5c4e105692f5a8ee377ce5c26cac632c14
Simon Oppowa <24407484+simonoppowa@users.noreply.github.com>
2022-09-23 14:10:51 +0200

    Added support for german language

--- lib/generated/intl/messages_all.dart
+++ lib/generated/intl/messages_all.dart
@@ -15,15 +15,19 @@ import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
 import 'package:intl/src/intl_helpers.dart';
 
+import 'messages_de.dart' as messages_de;
 import 'messages_en.dart' as messages_en;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
+  'de': () => new Future.value(null),
   'en': () => new Future.value(null),
 };
 
 MessageLookupByLibrary? _findExact(String localeName) {
   switch (localeName) {
+    case 'de':
+      return messages_de.messages;
     case 'en':
       return messages_en.messages;
     default:
--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -67,6 +67,7 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'de'),
     ];
   }
 
