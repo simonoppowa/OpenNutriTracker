557944b454bd9e60447db554721cf7815cc52f30
Simon Oppowa <24407484+simonoppowa@users.noreply.github.com>
2022-09-29 22:08:34 +0200

    Added border to item detail bottom sheet

--- lib/features/item_detail/presentation/widgets/item_detail_bottom_sheet.dart
+++ lib/features/item_detail/presentation/widgets/item_detail_bottom_sheet.dart
@@ -15,56 +15,70 @@ class _ItemDetailBottomSheetState extends State<ItemDetailBottomSheet> {
         elevation: 10,
         onClosing: () {},
         builder: (context) {
-          return Wrap(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 32.0, 16.0, 8.0),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            initialValue: '100',
-                            keyboardType: TextInputType.number,
-                            decoration: InputDecoration(
-                              border: const OutlineInputBorder(),
-                              labelText: S.of(context).quantityLabel,
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).colorScheme.outline,
+                width: 0.5,
+              ),
+              color: Colors.white,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(32),
+                topRight: Radius.circular(32),
+              ),
+            ),
+            child: Wrap(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 32.0, 16.0, 8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              initialValue: '100',
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                border: const OutlineInputBorder(),
+                                labelText: S.of(context).quantityLabel,
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(width: 16.0),
-                        Expanded(
-                            child: DropdownButtonFormField(
-                          decoration: InputDecoration(
-                              border: const OutlineInputBorder(),
-                              labelText: S.of(context).unitLabel),
-                          items: const <DropdownMenuItem<String>>[
-                            DropdownMenuItem(child: Text('g'))
-                          ],
-                          onChanged: (Object? value) {},
-                        ))
-                      ],
-                    ),
-                    SizedBox(
-                      width: double.infinity, // Make button full width
-                      child: ElevatedButton.icon(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Theme.of(context)
-                                .colorScheme
-                                .onPrimaryContainer,
-                            backgroundColor:
-                                Theme.of(context).colorScheme.primaryContainer,
-                          ).copyWith(
-                              elevation: ButtonStyleButton.allOrNull(0.0)),
-                          icon: const Icon(Icons.add_outlined),
-                          label: Text(S.of(context).addLabel)),
-                    ),
-                  ],
+                          const SizedBox(width: 16.0),
+                          Expanded(
+                              child: DropdownButtonFormField(
+                            decoration: InputDecoration(
+                                border: const OutlineInputBorder(),
+                                labelText: S.of(context).unitLabel),
+                            items: const <DropdownMenuItem<String>>[
+                              DropdownMenuItem(child: Text('g'))
+                            ],
+                            onChanged: (Object? value) {},
+                          ))
+                        ],
+                      ),
+                      SizedBox(
+                        width: double.infinity, // Make button full width
+                        child: ElevatedButton.icon(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              foregroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .onPrimaryContainer,
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
+                            ).copyWith(
+                                elevation: ButtonStyleButton.allOrNull(0.0)),
+                            icon: const Icon(Icons.add_outlined),
+                            label: Text(S.of(context).addLabel)),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           );
         });
   }
