2f4a5c558deadec69e06c05b8ecce4874821f032
Simon Oppowa <24407484+simonoppowa@users.noreply.github.com>
2022-10-24 22:32:20 +0200

    Added user box to hive db

--- lib/core/utils/hive_db_provider.dart
+++ lib/core/utils/hive_db_provider.dart
@@ -4,11 +4,17 @@ import 'package:opennutritracker/core/data/dbo/intake_dbo.dart';
 import 'package:opennutritracker/core/data/dbo/intake_type_dbo.dart';
 import 'package:opennutritracker/core/data/dbo/product_dbo.dart';
 import 'package:opennutritracker/core/data/dbo/product_nutriments_dbo.dart';
+import 'package:opennutritracker/core/data/dbo/user_dbo.dart';
+import 'package:opennutritracker/core/data/dbo/user_gender_dbo.dart';
+import 'package:opennutritracker/core/data/dbo/user_pal_dbo.dart';
+import 'package:opennutritracker/core/data/dbo/user_weight_goal_dbo.dart';
 
 class HiveDBProvider extends ChangeNotifier{
-  static const userBoxName = 'IntakeBox';
+  static const intakeBoxName = 'IntakeBox';
+  static const userBoxName = 'UserBox';
 
   late Box<IntakeDBO> intakeBox;
+  late Box<UserDBO> userBox;
 
   Future<void> initHiveDB() async {
     await Hive.initFlutter();
@@ -16,7 +22,13 @@ class HiveDBProvider extends ChangeNotifier{
     Hive.registerAdapter(ProductDBOAdapter());
     Hive.registerAdapter(ProductNutrimentsDBOAdapter());
     Hive.registerAdapter(IntakeTypeDBOAdapter());
-    intakeBox = await Hive.openBox(userBoxName);
+    Hive.registerAdapter(UserDBOAdapter());
+    Hive.registerAdapter(UserGenderDBOAdapter());
+    Hive.registerAdapter(UserWeightGoalDBOAdapter());
+    Hive.registerAdapter(UserPALDBOAdapter());
+
+    intakeBox = await Hive.openBox(intakeBoxName);
+    userBox = await Hive.openBox(userBoxName);
   }
 
 
