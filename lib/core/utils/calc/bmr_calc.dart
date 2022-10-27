ae584ca60700c310cbb5e6740720e6954bc1766c
Simon Oppowa <24407484+simonoppowa@users.noreply.github.com>
2022-10-27 22:44:42 +0200

    Added Harris-Benedict bmr equations

---
+++ lib/core/utils/calc/bmr_calc.dart
@@ -0,0 +1,57 @@
+import 'package:opennutritracker/core/domain/entity/user_entity.dart';
+import 'package:opennutritracker/core/domain/entity/user_gender_entity.dart';
+
+///
+/// Calculates BMR of UserEntity based on the 1918 Harris-Benedict equation
+/// from the paper 'A Biometric Study of Human Basal Metabolism'
+/// by Harris & Benedict
+/// https://pubmed.ncbi.nlm.nih.gov/16576330/
+///
+class BMRCalc {
+  static double getBMRHarrisBenedict1918(UserEntity user) {
+    double bmr;
+    switch (user.gender) {
+      case UserGenderEntity.male:
+        bmr = 66.4730 +
+            13.7516 * user.weightKG +
+            5.0033 * user.heightCM -
+            6.7550 * user.age;
+        break;
+      case UserGenderEntity.female:
+        bmr = 655.0955 +
+            9.5634 * user.weightKG +
+            1.8496 * user.heightCM -
+            4.6756 * user.age;
+        break;
+    }
+    return bmr;
+  }
+
+  ///
+  /// Calculates BMR of UserEntity based on the 1984 revised
+  /// Harris-Benedict equation from the paper
+  /// 'The Harris Benedict equation reevaluated: resting energy
+  /// requirements and the body cell mass' by Roza & Shizgal
+  /// https://academic.oup.com/ajcn/article-abstract/40/1/168/4691315
+  ///
+  static double getBMRRevisedHarrisBenedict1984(UserEntity user) {
+    double bmr;
+    switch (user.gender) {
+      case UserGenderEntity.male:
+        bmr = 88.362 +
+            13.397 * user.weightKG +
+            4.799 * user.heightCM -
+            5.677 * user.age;
+        break;
+      case UserGenderEntity.female:
+        bmr = 447.593 +
+            9.247 * user.weightKG +
+            3.098 * user.heightCM -
+            4.330 * user.age;
+        break;
+    }
+    return bmr;
+  }
+
+
+}
