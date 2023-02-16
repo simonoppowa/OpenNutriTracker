46087d966471984208d5c61371b53f8a64bd57f5
Simon Oppowa <24407484+simonoppowa@users.noreply.github.com>
2023-02-16 21:42:40 +0100

    Excluded app folders from icloud backup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,15 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+        // Exclude the documents folder from iCloud backup.
+        try! setExcludeFromiCloudBackup(isExcluded: true)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+
+private func setExcludeFromiCloudBackup(isExcluded: Bool) throws {
+    var fileOrDirectoryURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    var values = URLResourceValues()
+    values.isExcludedFromBackup = isExcluded
+    try fileOrDirectoryURL.setResourceValues(values)
 }
