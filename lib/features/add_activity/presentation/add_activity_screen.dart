d5dbc91ffbe5eb13f7024adbf0a107cd38b38f9f
Simon Oppowa <24407484+simonoppowa@users.noreply.github.com>
2023-05-24 15:52:05 +0200

    Added search for activities

--- lib/features/add_activity/presentation/add_activity_screen.dart
+++ lib/features/add_activity/presentation/add_activity_screen.dart
@@ -13,11 +13,14 @@ class AddActivityScreen extends StatefulWidget {
 
 class _AddActivityScreenState extends State<AddActivityScreen> {
   final ActivitiesBloc _activitiesBloc = ActivitiesBloc();
+
   late List<PhysicalActivityEntity> _physicalActivities;
+  late List<PhysicalActivityEntity> _activitySuggestions;
 
   @override
   void didChangeDependencies() {
     _physicalActivities = _activitiesBloc.getPhysicalActivity(context);
+    _activitySuggestions = _physicalActivities;
     super.didChangeDependencies();
   }
 
@@ -29,15 +32,48 @@ class _AddActivityScreenState extends State<AddActivityScreen> {
         ),
         body: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    hintText: S.of(context).searchLabel,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+                onChanged: _onSearchTextChanged,
+              ),
+            ),
+            const SizedBox(height: 16.0),
             Flexible(
               child: ListView.builder(
-                  itemCount: _physicalActivities.length,
+                  itemCount: _activitySuggestions.length,
                   itemBuilder: (context, index) {
                     return ActivityItemCard(
-                        physicalActivityEntity: _physicalActivities[index]);
+                        physicalActivityEntity: _activitySuggestions[index]);
                   }),
             ),
           ],
         ));
+  }
+
+  void _onSearchTextChanged(String query) {
+    setState(() {
+      if (query == "") {
+        _activitySuggestions = _physicalActivities;
+      } else {
+        final formattedQuery = query.toLowerCase();
+        _activitySuggestions = _physicalActivities.where((activity) {
+          final formattedActivityName = activity.getName(context).toLowerCase();
+          final formattedActivityDescription =
+              activity.getDescription(context).toLowerCase();
+          final containsQuery =
+              formattedActivityName.contains(formattedQuery) ||
+                  formattedActivityDescription.contains(formattedQuery);
+
+          return containsQuery;
+        }).toList();
+      }
+    });
   }
 }
