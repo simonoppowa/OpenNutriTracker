f46c422d84e5f7887c60d4bbad430da77bc43301
JEdits20 <jaanerik.sieverding@gmail.com>
2025-05-17 15:12:32 +0200

    Added weight validation to the profile_page.dart

--- lib/features/profile/presentation/widgets/set_weight_dialog.dart
+++ lib/features/profile/presentation/widgets/set_weight_dialog.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:horizontal_picker/horizontal_picker.dart';
+import 'package:opennutritracker/core/utils/bounds/ranges_const.dart';
+import 'package:opennutritracker/core/utils/bounds/validator.dart';
 import 'package:opennutritracker/generated/l10n.dart';
 
 class SetWeightDialog extends StatelessWidget {
-  static const weightRangeKg = 50.0;
-  static const weightRangeLbs = 100.0;
+  static const divisionsPerLbs = 5;
+  static const divisionsPerKg = 10;
+  static const kgScrollBuffer = 2;
+  static const lbsScrollBuffer = 5;
 
   final double userWeight;
   final bool usesImperialUnits;
+  late final int divisions;
+  late final double maxWeight, minWeight;
 
-  const SetWeightDialog(
-      {super.key, required this.userWeight, required this.usesImperialUnits});
+
+  SetWeightDialog(this.userWeight, this.usesImperialUnits, {super.key})
+      {
+        super.key;
+        divisions = calculateDivisions();
+      }
+
+  int calculateDivisions(){
+    getCloserBound();
+    return usesImperialUnits
+        ? ((maxWeight - minWeight) * divisionsPerLbs).round()
+        : ((maxWeight - minWeight) * divisionsPerKg).round();
+  }
+
+  void getCloserBound(){
+    double initialMaxWeight = usesImperialUnits ? Ranges.maxWeightInLbs : Ranges.maxWeight;
+    double initialMinWeight = usesImperialUnits ? Ranges.minWeightInLbs : Ranges.minWeight;
+    initialMinWeight = initialMinWeight - (usesImperialUnits ? lbsScrollBuffer : kgScrollBuffer);
+    initialMaxWeight = initialMaxWeight + (usesImperialUnits ? lbsScrollBuffer : kgScrollBuffer);
+    if(initialMaxWeight - userWeight > userWeight - initialMinWeight){
+      print("sehr leicht");
+      minWeight = initialMinWeight;
+      maxWeight = userWeight + (userWeight - minWeight);
+    }else{
+      print("sehr schwer");
+      maxWeight = initialMaxWeight;
+      minWeight = userWeight - (maxWeight - userWeight);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,14 +56,10 @@ class SetWeightDialog extends StatelessWidget {
             HorizontalPicker(
                 height: 100,
                 backgroundColor: Colors.transparent,
-                minValue: usesImperialUnits
-                    ? userWeight - weightRangeLbs
-                    : userWeight - weightRangeKg,
-                maxValue: usesImperialUnits
-                    ? userWeight + weightRangeLbs
-                    : userWeight + weightRangeKg,
+                minValue: minWeight,
+                maxValue: maxWeight,
                 initialPosition: InitialPosition.center,
-                divisions: 1000,
+                divisions:  divisions,
                 suffix: usesImperialUnits
                     ? S.of(context).lbsLabel
                     : S.of(context).kgLabel,
@@ -48,8 +77,10 @@ class SetWeightDialog extends StatelessWidget {
             child: Text(S.of(context).dialogCancelLabel)),
         TextButton(
             onPressed: () {
-              // TODO validate selected weight
-              Navigator.pop(context, selectedWeight);
+              double? weight = ValueValidator.parseWeightInKg(selectedWeight, isImperial: usesImperialUnits);
+              if(weight != null) Navigator.pop(context, weight);
+              else ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(S.of(context).onboardingWrongWeightLabel)));
             },
             child: Text(S.of(context).dialogOKLabel)),
       ],
