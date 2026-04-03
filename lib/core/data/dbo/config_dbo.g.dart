df703e94fc9813656fa3b8dc2594af0a1ad132eb
manum45 <manu.707@web.de>
2026-04-04 01:02:10 +0200

    generate file instead of modifying manually

--- lib/core/data/dbo/config_dbo.g.dart
+++ lib/core/data/dbo/config_dbo.g.dart
@@ -23,12 +23,12 @@ class ConfigDBOAdapter extends TypeAdapter<ConfigDBO> {
       fields[3] as AppThemeDBO,
       usesImperialUnits: fields[4] as bool?,
       userKcalAdjustment: fields[5] as double?,
+      useLocalDataBase: fields[9] as bool?,
+      localDatabaseFile: fields[10] as String?,
     )
       ..userCarbGoalPct = fields[6] as double?
       ..userProteinGoalPct = fields[7] as double?
-      ..userFatGoalPct = fields[8] as double?
-      ..useLocalDataBase = fields[9] as bool?
-      ..localDatabaseFile = fields[10] as String?;
+      ..userFatGoalPct = fields[8] as double?;
   }
 
   @override
@@ -81,8 +81,8 @@ ConfigDBO _$ConfigDBOFromJson(Map<String, dynamic> json) => ConfigDBO(
       $enumDecode(_$AppThemeDBOEnumMap, json['selectedAppTheme']),
       usesImperialUnits: json['usesImperialUnits'] as bool? ?? false,
       userKcalAdjustment: (json['userKcalAdjustment'] as num?)?.toDouble(),
-      useLocalDataBase: json['useLocalDataBase'] as bool,
-      localDatabaseFile: json['localDatabaseFile'] as String?
+      useLocalDataBase: json['useLocalDataBase'] as bool? ?? false,
+      localDatabaseFile: json['localDatabaseFile'] as String?,
     )
       ..userCarbGoalPct = (json['userCarbGoalPct'] as num?)?.toDouble()
       ..userProteinGoalPct = (json['userProteinGoalPct'] as num?)?.toDouble()
@@ -92,14 +92,14 @@ Map<String, dynamic> _$ConfigDBOToJson(ConfigDBO instance) => <String, dynamic>{
       'hasAcceptedDisclaimer': instance.hasAcceptedDisclaimer,
       'hasAcceptedPolicy': instance.hasAcceptedPolicy,
       'hasAcceptedSendAnonymousData': instance.hasAcceptedSendAnonymousData,
-      'useLocalDataBase': instance.useLocalDataBase,
-      'localDatabaseFile': instance.localDatabaseFile,
       'selectedAppTheme': _$AppThemeDBOEnumMap[instance.selectedAppTheme]!,
       'usesImperialUnits': instance.usesImperialUnits,
       'userKcalAdjustment': instance.userKcalAdjustment,
       'userCarbGoalPct': instance.userCarbGoalPct,
       'userProteinGoalPct': instance.userProteinGoalPct,
       'userFatGoalPct': instance.userFatGoalPct,
+      'useLocalDataBase': instance.useLocalDataBase,
+      'localDatabaseFile': instance.localDatabaseFile,
     };
 
 const _$AppThemeDBOEnumMap = {
